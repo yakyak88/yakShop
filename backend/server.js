e6f7806a85e3093d70913b2d3803f049dfa394b4
yakir <danielyakir01@gmail.com>
2022-09-27 20:11:36 +0300

    get ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,15 @@ app.use(cors());
 app.use("/api/products", ProductRoutes);
 app.use("/api/users", UsersRoutes);
 app.use("/api/orders", OrderRoutes);
-app.use(notFound);
-app.use(errorHandler);
+//app.use(notFound);
+//app.use(errorHandler);
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.use(express.static(path.join(__dirname, "/fronthead/build")));
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(
+            path.resolve(__dirname, "fronthead", "build", "index.html")
+        )
     );
 } else {
     app.get("/", (req, res) => {
